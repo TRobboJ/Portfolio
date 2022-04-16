14ea308df22d17713f0e1a99cc52a04f9bcba1fe
Thomas R <90433088+TRobboJ@users.noreply.github.com>
2022-04-16 21:23:54 +0900

    Cover edits

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -13,40 +13,40 @@ const toggleHamburgerMenu = function(){
 
 hamburgerIcon.addEventListener("click", toggleHamburgerMenu)
 // Smooth scrolling for sections
-// document.querySelector('.nav__list').addEventListener('click', function (e) {
-//       e.preventDefault();
-      
-//       if (e.target.classList.contains('nav__link')) {
-            
-//             const id = e.target.getAttribute('href');
-//             document.querySelector(id).scrollIntoView({ behavior: 'smooth'});
-            
-//       }
-//       });
-
-
-
 document.querySelector('.nav__list').addEventListener('click', function (e) {
       e.preventDefault();
-      toggleHamburgerMenu()
+      
       if (e.target.classList.contains('nav__link')) {
-            // const header = document.querySelector(`.header`)
-            // const headerHeight = header.offsetHeight
+            
             const id = e.target.getAttribute('href');
-            const element = document.querySelector(id)
-            const elementPosition = element.getBoundingClientRect().top
-            const offset = elementPosition  - window.innerHeight  + 93
+            document.querySelector(id).scrollIntoView({ behavior: 'smooth'});
+            
+      }
+      });
+
+
+
+// document.querySelector('.nav__list').addEventListener('click', function (e) {
+//       e.preventDefault();
+//       toggleHamburgerMenu()
+//       if (e.target.classList.contains('nav__link')) {
+//             // const header = document.querySelector(`.header`)
+//             // const headerHeight = header.offsetHeight
+//             const id = e.target.getAttribute('href');
+//             const element = document.querySelector(id)
+//             const elementPosition = element.getBoundingClientRect().top
+//             const offset = elementPosition  - window.innerHeight  + 93
             
         
-            window.scrollTo({
-                  top: offset,
-                  behavior: 'smooth'
-            })
+//             window.scrollTo({
+//                   top: offset,
+//                   behavior: 'smooth'
+//             })
             
-            setTimeout(()=>{
-                  const hTest = document.querySelector(`.header`)
-                  const hHtest = hTest.offsetHeight
-                  console.log(hHtest)}, 10000)
-      }
+//             setTimeout(()=>{
+//                   const hTest = document.querySelector(`.header`)
+//                   const hHtest = hTest.offsetHeight
+//                   console.log(hHtest)}, 10000)
+//       }
       
-      });
+//       });
