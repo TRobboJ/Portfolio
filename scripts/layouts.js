8a9267e754673e895f359a83f0b6604481b270a1
Thomas R <90433088+TRobboJ@users.noreply.github.com>
2022-04-16 11:41:16 +0900

    600px and 900px css

--- scripts/layouts.js
+++ scripts/layouts.js
@@ -1,23 +1,35 @@
-const EN_WELCOME = 'Welcome!'
-const JA_WELCOME = 'ようこそ!'
-const TEXT_SPEED = 300
+const EN_WELCOME = `Welcome!`
+const JA_WELCOME = `ようこそ!`
+const EN_HERO = `My name is Thomas and I'm a web developer.`
+const TEXT_SPEED_WELCOME = 300
+const TEXT_SPEED_HERO = 50
 const welcomeText = document.querySelector("#welcome__text")
+const heroText = document.querySelector("#hero__text")
 
 let i = 0;
+let x = 0
 
 // Welcome screen typewriter text
 function typeWriter() {
-   if (i < EN_WELCOME.length) {
+  if (i < EN_WELCOME.length) {
     welcomeText.innerHTML += EN_WELCOME.charAt(i);
     i++;
-    setTimeout(typeWriter, TEXT_SPEED)
-    
+    setTimeout(typeWriter, TEXT_SPEED_WELCOME);
+  }
   
+}
+function typeWriterHero() {
+  if (x < EN_HERO.length) {
+    heroText.innerHTML += EN_HERO.charAt(x);
+    x++;
+    setTimeout(typeWriterHero, TEXT_SPEED_HERO);
+  }
   
 }
   
-}
+
 typeWriter()
+typeWriterHero()
 
 function typeWriterJA() {
     
@@ -27,4 +39,5 @@ function typeWriterJA() {
       setTimeout(typeWriterJA, TEXT_SPEED);
     }
   }
+
 
